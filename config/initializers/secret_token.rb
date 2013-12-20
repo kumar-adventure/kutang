3fecc7a86851d09ead13fe32a0684efa0c0a6394
kumar <kumar.adventure@gmail.com>
2013-12-20 20:17:45 +0530

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kutang::Application.config.secret_token = 'de681ad561b1fe7c6b3d5bcd96ba4d20fef555dc54697850a92d79f51cbbb168f4ffdf23f42f38827d863820cb36a0b06f9f3c838393e268f6e4e30c348493d4'
